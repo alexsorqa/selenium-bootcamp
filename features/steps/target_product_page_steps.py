8680dcf9d0761aa87352c90c280e05aa8cd01099
Alexey Sorokin <asorokin@Alexeys-MacBook-Air.local>
2024-12-23 20:43:19 -0800

    Homework 5 new submission

--- features/steps/target_product_page_steps.py
+++ features/steps/target_product_page_steps.py
@@ -5,6 +5,8 @@ from selenium.webdriver.support import expected_conditions as EC
 
 
 COLORS = (By.XPATH, "//div[@data-test='@web/VariationComponent']//ul/li")
+COLOR_OPTIONS = (By.CSS_SELECTOR, "div[aria-label='Carousel'] li img")
+SELECTED_COLOR = (By.CSS_SELECTOR, "[data-test='@web/VariationComponent'] div")
 
 
 @then('{product} colors are visible')
@@ -16,3 +18,17 @@ def product_colors_visible(context, product):
     main_color = sorted(['Starlight Blue', 'Galactic Purple', 'Midnight Black'])
     total = sorted(list(filter(lambda x: x in main_color, total_colors)))
     assert main_color == total, f"Expected {main_color} but got {total}"
+
+
+@given('Open shoes page')
+def open_shoes_page(context):
+    context.driver.get('https://www.target.com/p/A-91511634')
+
+
+@then('Select different colors')
+def select_different_colors(context):
+    item_colors = context.driver.find_elements(*COLOR_OPTIONS)
+    for color in item_colors:
+        color.click()
+        selected_color = context.driver.find_elements(*SELECTED_COLOR)[2].text
+        print('Current color:', selected_color)
--- features/tests/Homework_5.feature
+++ features/tests/Homework_5.feature
@@ -6,3 +6,7 @@ Feature: Homework 5
     Then DualSense Wireless Controller for PlayStation 5 result is displayed
     Then Click on DualSense Wireless Controller for PlayStation 5 in results page
     And DualSense Wireless Controller for PlayStation 5 colors are visible
+
+  Scenario: Item color selection
+    Given Open shoes page
+    Then Select different colors
